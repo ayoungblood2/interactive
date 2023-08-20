a9a020e3865f7cb1c92ad16a2a9a3ae84873bc4e
ayoungblood2 <60159318+ayoungblood2@users.noreply.github.com>
2023-08-19 21:11:05 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 
 
-console.log("this is avery's website. things i need to fix: mouse position, ticker tape responsive, margin bottom text consistency, unicode eyes appear if no activity, more images when clicking or hovering over preview img, are.na cms for images, custom website email, different custom domain, two column info/about box, timestamp")
+console.log("this is avery's website. things i need to fix: mouse position, ticker tape responsive, margin bottom text consistency, unicode eyes appear if no activity, more images when clicking or hovering over preview img, are.na cms for images, custom website email, different custom domain, two column info/about box, current timestamp, inverse button with black bkgrnd like arc browser")
 
 //about text container
 let about = document.querySelector(".about");
